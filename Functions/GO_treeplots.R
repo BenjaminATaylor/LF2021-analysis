6758576b4f337b25d689c9db0ffc8093dc393423
BenjaminATaylor <benjamin.aaron.taylor@gmail.com>
2021-01-20 12:03:31 +0000

    Fix formatting when onyology missing + increase aspect ratio

--- Functions/GO_treeplots.R
+++ Functions/GO_treeplots.R
@@ -5,16 +5,25 @@ GO_treeplots = function(GOframe, path){
   require(treemap)
   require(grid)
   require(GOSemSim)
+  require(RColorBrewer)
   
   #create similarity matrices in parent environment if necessary
   if(!exists("BPsim")){BPsim <<- GOSemSim::godata(org.Dm.eg.db, ont="BP")}
   if(!exists("MFsim")){MFsim <<- GOSemSim::godata(org.Dm.eg.db, ont="MF")}
   if(!exists("CCsim")){CCsim <<- GOSemSim::godata(org.Dm.eg.db, ont="CC")}
   
+  #check numbers of terms in each ontology
+  BPTRUE = nrow(subset(GOframe$consolidated_result, Ontology == "BP"))>1
+  MFTRUE = nrow(subset(GOframe$consolidated_result, Ontology == "MF"))>1
+  CCTRUE = nrow(subset(GOframe$consolidated_result, Ontology == "CC"))>1
+  TRUEsum = sum(BPTRUE,CCTRUE,MFTRUE)
+  
   #prepare plotting environment
-  png(filename=path, width=15, height=45, units = "cm", res = 800)
+  png(filename=path, width=22, height=45*(TRUEsum/3), units = "cm", res = 800)
   grid.newpage()
-  pushViewport(viewport(layout=grid.layout(3, 1)))
+  pushViewport(viewport(layout=grid.layout(TRUEsum, 1)))
+  row = 1
+  aspR = 1.5
   
   #BP
   simMatrix <- suppressWarnings(calculateSimMatrix(GOframe$consolidated_result$GO.ID,
@@ -23,7 +32,7 @@ GO_treeplots = function(GOframe, path){
                                                    ont="BP",
                                                    method="Rel"))
   scores <- setNames(-log10(as.numeric(GOframe$consolidated_result$result)), GOframe$consolidated_result$GO.ID)
-  if(length(simMatrix)<2){print("Insufficient GO terms for plotting in ontology: BP")
+  if(!BPTRUE){print("Insufficient GO terms for plotting in ontology: BP")
   } else {
   reducedTerms <- reduceSimMatrix(simMatrix,
                                   scores,
@@ -37,7 +46,9 @@ GO_treeplots = function(GOframe, path){
                                     fontcolor.labels = c("black", "white"), fontface.labels = c("bold","italic"), fontsize.labels = c(18,8),
                                     bg.labels = 20, overlap.labels = 0.5,
                                     border.col = "#00000080", 
-                                    vp = vp, vsize = 1/3, aspRatio = 1))
+                                    vp = vp, vsize = 1/3, aspRatio = aspR))
+  #increment position
+  row = row + 1
   }
   
   #MF
@@ -46,13 +57,13 @@ GO_treeplots = function(GOframe, path){
                                                    ont="MF",
                                                    method="Rel"))
   scores <- setNames(-log10(as.numeric(GOframe$consolidated_result$result)), GOframe$consolidated_result$GO.ID)
-  if(length(simMatrix)<2){print("Insufficient GO terms for plotting in ontology: MF")
+  if(!MFTRUE){print("Insufficient GO terms for plotting in ontology: MF")
   } else {
   reducedTerms <- reduceSimMatrix(simMatrix,
                                   scores,
                                   threshold=0.9,
                                   orgdb="org.Dm.eg.db")
-  vp <- viewport(layout.pos.col=1, layout.pos.row=2)
+  vp <- viewport(layout.pos.col=1, layout.pos.row=row)
   suppressWarnings(treemap::treemap(reducedTerms, index = c("parentTerm", "term"), 
                                     vSize = "score", type = "index", 
                                     title = "Molecular Function", fontsize.title = 18,
@@ -60,7 +71,8 @@ GO_treeplots = function(GOframe, path){
                                     fontcolor.labels = c("black", "white"), fontface.labels = c("bold","italic"), fontsize.labels = c(18,12),
                                     bg.labels = 20, overlap.labels = 0.5,
                                     border.col = "#00000080", 
-                                    vp = vp, vsize = 1/3, aspRatio = 1))
+                                    vp = vp, vsize = 1/3, aspRatio = aspR))
+  row = row + 1
   }
   
   #CC
@@ -69,14 +81,14 @@ GO_treeplots = function(GOframe, path){
                                                    ont="CC",
                                                    method="Rel"))
   scores <- setNames(-log10(as.numeric(GOframe$consolidated_result$result)), GOframe$consolidated_result$GO.ID)
-  if(length(simMatrix)<2){print("Insufficient GO terms for plotting in ontology: CC")
+  if(!CCTRUE){print("Insufficient GO terms for plotting in ontology: CC")
     } else {
   reducedTerms <- reduceSimMatrix(simMatrix,
                                   scores,
                                   threshold=0.9,
                                   orgdb="org.Dm.eg.db")
     
-  vp <- viewport(layout.pos.col=1, layout.pos.row=3)
+  vp <- viewport(layout.pos.col=1, layout.pos.row=row)
   suppressWarnings(treemap::treemap(reducedTerms, index = c("parentTerm", "term"),  
                                     vSize = "score", type = "index", 
                                     title = "Cellular Component", fontsize.title = 18,
@@ -84,8 +96,10 @@ GO_treeplots = function(GOframe, path){
                                     fontcolor.labels = c("black", "white"), fontface.labels = c("bold","italic"), fontsize.labels = c(18,12),
                                     bg.labels = 20, overlap.labels = 0.5,
                                     border.col = "#00000080", 
-                                    vp = vp, vsize = 1/3, aspRatio = 1))
+                                    vp = vp, vsize = 1/3, aspRatio = aspR))
     }
   dev.off()
   
 }
+
+GO_treeplots(darkturquoiseGO, path = "/home/benjamin/Desktop/treeplot_temp.png")
